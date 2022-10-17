7f5c9c58679f95af5db13726d1551bbed78ef853
Tobias Schramm <tobleminer@gmail.com>
2022-10-17 02:35:23 +0200

    smbus: support block read/write

--- main/smbus.c
+++ main/smbus.c
@@ -72,3 +72,81 @@ fail_link:
 fail:
 	return err;
 }
+
+esp_err_t smbus_write_block(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data, size_t len) {
+	esp_err_t err;
+	size_t cmd_buf_len = I2C_LINK_RECOMMENDED_SIZE(4);
+	void *cmd_buf = malloc(cmd_buf_len);
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create_static(cmd_buf, cmd_buf_len);
+	if(!cmd) {
+		err = ESP_ERR_NO_MEM;
+		goto fail;
+	}
+	if((err = i2c_master_start(cmd))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write_byte(cmd, slave << 1, 1))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write_byte(cmd, smcmd, 1))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write_byte(cmd, len, 1))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write(cmd, data, len, true))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_stop(cmd))) {
+		goto fail_link;
+	}
+	err = i2c_bus_cmd_begin(bus, cmd, pdMS_TO_TICKS(1000));
+fail_link:
+	i2c_cmd_link_delete_static(cmd);
+fail:
+	return err;
+}
+
+esp_err_t smbus_read_block(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data, size_t len, size_t *data_len) {
+	esp_err_t err;
+	size_t cmd_buf_len = I2C_LINK_RECOMMENDED_SIZE(5);
+	void *cmd_buf = malloc(cmd_buf_len);
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create_static(cmd_buf, cmd_buf_len);
+	if(!cmd) {
+		err = ESP_ERR_NO_MEM;
+		goto fail;
+	}
+	if((err = i2c_master_start(cmd))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write_byte(cmd, slave << 1, 1))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write_byte(cmd, smcmd, 1))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_start(cmd))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_write_byte(cmd, (slave << 1) | 1, 1))) {
+		goto fail_link;
+	}
+	uint8_t data_len_;
+	if((err = i2c_master_read_byte(cmd, &data_len_, I2C_MASTER_ACK))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_read(cmd, (uint8_t*)data, len, I2C_MASTER_LAST_NACK))) {
+		goto fail_link;
+	}
+	if((err = i2c_master_stop(cmd))) {
+		goto fail_link;
+	}
+	err = i2c_bus_cmd_begin(bus, cmd, pdMS_TO_TICKS(1000));
+	if (!err && data_len) {
+		*data_len = data_len_;
+	}
+fail_link:
+	i2c_cmd_link_delete_static(cmd);
+fail:
+	return err;
+}
--- main/smbus.h
+++ main/smbus.h
@@ -6,6 +6,8 @@
 
 esp_err_t smbus_read(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data, size_t len);
 esp_err_t smbus_write(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data, size_t len);
+esp_err_t smbus_write_block(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data, size_t len);
+esp_err_t smbus_read_block(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data, size_t len, size_t *data_len);
 
 static inline esp_err_t smbus_read_byte(struct i2c_bus* bus, uint8_t slave, uint8_t smcmd, void *data) {
 	return smbus_read(bus, slave, smcmd, data, 1);
