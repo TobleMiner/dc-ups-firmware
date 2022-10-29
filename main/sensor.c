2b262cc4d692bab90ccecf634db8278270e8e275
Tobias Schramm <tobias.schramm@go-e.co>
2022-10-29 13:08:28 +0200

    sensor: encode sensor type into metric and metric value `priv` member

--- main/sensor.c
+++ main/sensor.c
@@ -4,11 +4,19 @@
 
 #include "util.h"
 
+#define METRIC_PRIV(type_) ((void *)((unsigned int)(type_) & 0xf))
+#define METRIC_PRIV_TYPE(priv_) ((sensor_measurement_type_t)((unsigned int)(priv_) & 0xf))
+
+#define VALUE_PRIV(index_, type_) ((void *)(((unsigned int)(index_) << 4) | ((unsigned int)(type_) & 0xf)))
+#define VALUE_PRIV_INDEX(priv_) ((unsigned int)(priv_) >> 4)
+#define VALUE_PRIV_TYPE(priv_) ((sensor_measurement_type_t)((unsigned int)(priv_) & 0xf))
+
 static const char *TAG = "sensor";
 
 static DECLARE_LIST_HEAD(sensors);
 
-static unsigned int get_num_values_by_type(sensor_measurement_type_t type) {
+static unsigned int get_num_values(prometheus_metric_t *metric) {
+	sensor_measurement_type_t type = METRIC_PRIV_TYPE(metric->priv);
 	sensor_t *sensor;
 	unsigned int num_values = 0;
 	LIST_FOR_EACH_ENTRY(sensor, &sensors, list) {
@@ -32,8 +40,9 @@ static sensor_t *get_sensor_and_channel_by_type_and_index(sensor_measurement_typ
 	return NULL;
 }
 
-static unsigned int get_num_labels_by_type(const prometheus_metric_value_t *val, prometheus_metric_t *metric, sensor_measurement_type_t type) {
-	unsigned int value_index = (unsigned int)val->priv;
+static unsigned int get_num_labels(const prometheus_metric_value_t *val, prometheus_metric_t *metric) {
+	unsigned int value_index = VALUE_PRIV_INDEX(val->priv);
+	sensor_measurement_type_t type = VALUE_PRIV_TYPE(val->priv);
 	unsigned int channel;
 	sensor_t *sensor = get_sensor_and_channel_by_type_and_index(type, value_index, &channel);
 	if (sensor->def->get_channel_name && sensor->def->get_channel_name(sensor, type, channel)) {
@@ -43,24 +52,9 @@ static unsigned int get_num_labels_by_type(const prometheus_metric_value_t *val,
 	}
 }
 
-static unsigned int get_num_labels_voltage(const prometheus_metric_value_t *val, prometheus_metric_t *metric) {
-	return get_num_labels_by_type(val, metric, SENSOR_TYPE_VOLTAGE);
-}
-
-static unsigned int get_num_labels_current(const prometheus_metric_value_t *val, prometheus_metric_t *metric) {
-	return get_num_labels_by_type(val, metric, SENSOR_TYPE_CURRENT);
-}
-
-static unsigned int get_num_labels_power(const prometheus_metric_value_t *val, prometheus_metric_t *metric) {
-	return get_num_labels_by_type(val, metric, SENSOR_TYPE_POWER);
-}
-
-static unsigned int get_num_labels_temperature(const prometheus_metric_value_t *val, prometheus_metric_t *metric) {
-	return get_num_labels_by_type(val, metric, SENSOR_TYPE_TEMPERATURE);
-}
-
-static void get_label_by_type(const prometheus_metric_value_t *val, prometheus_metric_t *metric, unsigned int index, char *label, char *value, sensor_measurement_type_t type) {
-	unsigned int value_index = (unsigned int)val->priv;
+static void get_label(const prometheus_metric_value_t *val, prometheus_metric_t *metric, unsigned int index, char *label, char *value) {
+	unsigned int value_index = VALUE_PRIV_INDEX(val->priv);
+	sensor_measurement_type_t type = VALUE_PRIV_TYPE(val->priv);
 	unsigned int channel;
 	sensor_t *sensor = get_sensor_and_channel_by_type_and_index(type, value_index, &channel);
 	if (index == 0) {
@@ -72,24 +66,9 @@ static void get_label_by_type(const prometheus_metric_value_t *val, prometheus_m
 	}
 }
 
-static void get_label_voltage(const prometheus_metric_value_t *val, prometheus_metric_t *metric, unsigned int index, char *label, char *value) {
-	get_label_by_type(val, metric, index, label, value, SENSOR_TYPE_VOLTAGE);
-}
-
-static void get_label_current(const prometheus_metric_value_t *val, prometheus_metric_t *metric, unsigned int index, char *label, char *value) {
-	get_label_by_type(val, metric, index, label, value, SENSOR_TYPE_CURRENT);
-}
-
-static void get_label_power(const prometheus_metric_value_t *val, prometheus_metric_t *metric, unsigned int index, char *label, char *value) {
-	get_label_by_type(val, metric, index, label, value, SENSOR_TYPE_POWER);
-}
-
-static void get_label_temperature(const prometheus_metric_value_t *val, prometheus_metric_t *metric, unsigned int index, char *label, char *value) {
-	get_label_by_type(val, metric, index, label, value, SENSOR_TYPE_TEMPERATURE);
-}
-
-static void get_value_by_type(const prometheus_metric_value_t *val, prometheus_metric_t *metric, char *value, sensor_measurement_type_t type) {
-	unsigned int value_index = (unsigned int)val->priv;
+static void get_value(const prometheus_metric_value_t *val, prometheus_metric_t *metric, char *value) {
+	unsigned int value_index = VALUE_PRIV_INDEX(val->priv);
+	sensor_measurement_type_t type = VALUE_PRIV_TYPE(val->priv);
 	unsigned int channel;
 	sensor_t *sensor = get_sensor_and_channel_by_type_and_index(type, value_index, &channel);
 	long res;
@@ -101,54 +80,12 @@ static void get_value_by_type(const prometheus_metric_value_t *val, prometheus_m
 	}
 }
 
-static void get_value_voltage(const prometheus_metric_value_t *val, prometheus_metric_t *metric, char *value) {
-	return get_value_by_type(val, metric, value, SENSOR_TYPE_VOLTAGE);
-}
-
-static void get_value_current(const prometheus_metric_value_t *val, prometheus_metric_t *metric, char *value) {
-	return get_value_by_type(val, metric, value, SENSOR_TYPE_CURRENT);
-}
-
-static void get_value_power(const prometheus_metric_value_t *val, prometheus_metric_t *metric, char *value) {
-	return get_value_by_type(val, metric, value, SENSOR_TYPE_POWER);
-}
-
-static void get_value_temperature(const prometheus_metric_value_t *val, prometheus_metric_t *metric, char *value) {
-	return get_value_by_type(val, metric, value, SENSOR_TYPE_TEMPERATURE);
-}
-
-static void get_metric_value_by_type(unsigned int index, prometheus_metric_value_t *value, sensor_measurement_type_t type) {
-	value->priv = (void *)index;
-	switch (type) {
-	case SENSOR_TYPE_VOLTAGE:
-		value->get_num_labels = get_num_labels_voltage;
-		value->get_label = get_label_voltage;
-		value->get_value = get_value_voltage;
-		break;
-	case SENSOR_TYPE_CURRENT:
-		value->get_num_labels = get_num_labels_current;
-		value->get_label = get_label_current;
-		value->get_value = get_value_current;
-		break;
-	case SENSOR_TYPE_POWER:
-		value->get_num_labels = get_num_labels_power;
-		value->get_label = get_label_power;
-		value->get_value = get_value_power;
-		break;
-	case SENSOR_TYPE_TEMPERATURE:
-		value->get_num_labels = get_num_labels_temperature;
-		value->get_label = get_label_temperature;
-		value->get_value = get_value_temperature;
-		break;
-	}
-}
-
-static unsigned int get_num_values_voltage(prometheus_metric_t *metric) {
-	return get_num_values_by_type(SENSOR_TYPE_VOLTAGE);
-}
-
-static void get_metric_value_voltage(prometheus_metric_t *metric, unsigned int index, prometheus_metric_value_t *value) {
-	get_metric_value_by_type(index, value, SENSOR_TYPE_VOLTAGE);
+static void get_metric_value(prometheus_metric_t *metric, unsigned int index, prometheus_metric_value_t *value) {
+	sensor_measurement_type_t type = METRIC_PRIV_TYPE(metric->priv);
+	value->priv = VALUE_PRIV(index, type);
+	value->get_num_labels = get_num_labels;
+	value->get_label = get_label;
+	value->get_value = get_value;
 }
 
 static const prometheus_metric_def_t voltage_metric_def = {
@@ -156,59 +93,35 @@ static const prometheus_metric_def_t voltage_metric_def = {
 	.help = "Voltage sensors",
 	.type = PROMETHEUS_METRIC_TYPE_GAUGE,
 	.num_values = 0,
-	.get_num_values = get_num_values_voltage,
-	.get_value = get_metric_value_voltage,
+	.get_num_values = get_num_values,
+	.get_value = get_metric_value,
 };
-
-static unsigned int get_num_values_current(prometheus_metric_t *metric) {
-	return get_num_values_by_type(SENSOR_TYPE_CURRENT);
-}
-
-static void get_metric_value_current(prometheus_metric_t *metric, unsigned int index, prometheus_metric_value_t *value) {
-	get_metric_value_by_type(index, value, SENSOR_TYPE_CURRENT);
-}
 
 static const prometheus_metric_def_t current_metric_def = {
 	.name = "current",
 	.help = "Current sensors",
 	.type = PROMETHEUS_METRIC_TYPE_GAUGE,
 	.num_values = 0,
-	.get_num_values = get_num_values_current,
-	.get_value = get_metric_value_current,
+	.get_num_values = get_num_values,
+	.get_value = get_metric_value,
 };
-
-static unsigned int get_num_values_power(prometheus_metric_t *metric) {
-	return get_num_values_by_type(SENSOR_TYPE_POWER);
-}
-
-static void get_metric_value_power(prometheus_metric_t *metric, unsigned int index, prometheus_metric_value_t *value) {
-	get_metric_value_by_type(index, value, SENSOR_TYPE_POWER);
-}
 
 static const prometheus_metric_def_t power_metric_def = {
 	.name = "power",
 	.help = "Power sensors",
 	.type = PROMETHEUS_METRIC_TYPE_GAUGE,
 	.num_values = 0,
-	.get_num_values = get_num_values_power,
-	.get_value = get_metric_value_power,
+	.get_num_values = get_num_values,
+	.get_value = get_metric_value,
 };
-
-static unsigned int get_num_values_temperature(prometheus_metric_t *metric) {
-	return get_num_values_by_type(SENSOR_TYPE_TEMPERATURE);
-}
-
-static void get_metric_value_temperature(prometheus_metric_t *metric, unsigned int index, prometheus_metric_value_t *value) {
-	get_metric_value_by_type(index, value, SENSOR_TYPE_TEMPERATURE);
-}
 
 static const prometheus_metric_def_t temperature_metric_def = {
 	.name = "temperature",
 	.help = "Temperature sensors",
 	.type = PROMETHEUS_METRIC_TYPE_GAUGE,
 	.num_values = 0,
-	.get_num_values = get_num_values_temperature,
-	.get_value = get_metric_value_temperature,
+	.get_num_values = get_num_values,
+	.get_value = get_metric_value,
 };
 
 static prometheus_metric_t metric_voltage;
@@ -223,10 +136,10 @@ void sensor_init(sensor_t *sensor, const sensor_def_t *def, const char *name) {
 }
 
 void sensor_install_metrics(prometheus_t *prometheus) {
-	prometheus_metric_init(&metric_voltage, &voltage_metric_def, NULL);
-	prometheus_metric_init(&metric_current, &current_metric_def, NULL);
-	prometheus_metric_init(&metric_power, &power_metric_def, NULL);
-	prometheus_metric_init(&metric_temperature, &temperature_metric_def, NULL);
+	prometheus_metric_init(&metric_voltage, &voltage_metric_def, METRIC_PRIV(SENSOR_TYPE_VOLTAGE));
+	prometheus_metric_init(&metric_current, &current_metric_def, METRIC_PRIV(SENSOR_TYPE_CURRENT));
+	prometheus_metric_init(&metric_power, &power_metric_def, METRIC_PRIV(SENSOR_TYPE_POWER));
+	prometheus_metric_init(&metric_temperature, &temperature_metric_def, METRIC_PRIV(SENSOR_TYPE_TEMPERATURE));
 	prometheus_add_metric(prometheus, &metric_voltage);
 	prometheus_add_metric(prometheus, &metric_current);
 	prometheus_add_metric(prometheus, &metric_power);
