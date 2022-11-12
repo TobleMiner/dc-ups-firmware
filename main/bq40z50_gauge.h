ca9776dbba551e59a9cb7e453608b9d5e9261247
Tobias Schramm <tobias.schramm@go-e.co>
2022-11-12 12:42:33 +0100

    bq40z50_gauge: remove remnants of old temperature sensor code

--- main/bq40z50_gauge.h
+++ main/bq40z50_gauge.h
@@ -4,12 +4,10 @@
 
 #include "smbus.h"
 #include "sensor.h"
-//#include "temperature_sensor.h"
 
 typedef struct bq40z50 {
 	smbus_t *bus;
 	unsigned int address;
-//	temperature_sensor_t sensor;
 	sensor_t sensor;
 } bq40z50_t;
 
